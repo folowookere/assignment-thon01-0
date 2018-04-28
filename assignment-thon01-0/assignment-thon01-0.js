88ae70eae925cc7eb1677d0ab22488e99432af87
Funmi <folowookere@yahoo.com>
2018-04-28 15:55:11 +0000

    I need practice with objects

---
+++ assignment-thon01-0/assignment-thon01-0.js
@@ -0,0 +1,22 @@
+var carDealership = {
+    type1:"sedan",
+    type2:"suv",
+    type3:"sports"
+    
+};
+
+function availability {
+     if (dealership.hasOwnProperty("sedan")) {
+    return "Yes, they are available";
+    else return "No, they aren't available";
+    
+      if (dealership.hasOwnProperty("suv")) {
+    return "Yes, they are available";
+    else return "No, they aren't available";
+    
+      if (dealership.hasOwnProperty("sports")) {
+    return "Yes, they are available";
+    else return "No, they aren't available";
+}
+
+// I obviously need practice :/
