c9420f2fb0ba44827e3e003e3f4fc1700ca5056f
Funmi <folowookere@yahoo.com>
2018-04-28 16:57:32 +0000

    Second run through

--- assignment-thon01-0/assignment-thon01-0.js
+++ assignment-thon01-0/assignment-thon01-0.js
@@ -1,22 +1,32 @@
 var carDealership = {
-    type1:"sedan",
-    type2:"suv",
-    type3:"sports"
+    "sedan": {
+      price:50,
+      availability:true,
+    },
+    "suv": {
+     price:50,
+      availability:true,
+    }
+    "sports":{
+      price:25,
+      availability:true,
+    }
     
 };
 
-function availability {
-     if (dealership.hasOwnProperty("sedan")) {
+function availability(carDealership) {
+     if (carDealership.availability("true")) {
     return "Yes, they are available";
     else return "No, they aren't available";
-    
-      if (dealership.hasOwnProperty("suv")) {
+     }
+      if (carDealership.availability("true")) {
     return "Yes, they are available";
-    else return "No, they aren't available";
-    
-      if (dealership.hasOwnProperty("sports")) {
+    else if return "No, they aren't available";
+      }
+      if (carDealership.availability("true")) {
     return "Yes, they are available";
     else return "No, they aren't available";
 }
+}
 
-// I obviously need practice :/
+
