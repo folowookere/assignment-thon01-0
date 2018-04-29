f19412f82d326101b4b9831da7f291fd0116d5b6
Funmi <folowookere@yahoo.com>
2018-04-29 00:50:08 +0000

    Almost there

--- assignment-thon01-0/assignment-thon01-0.js
+++ assignment-thon01-0/assignment-thon01-0.js
@@ -1,30 +1,39 @@
-function carDealership (val) {
-      var sedan = {
-        price:50,
-        availability:2
+function carDealership() {
+  var sedan = {
+         price:50,
+         availability:2
       };
-      if (sedan.availability > 0) {
-        return "Yes, they are available";
-}
-        else return "No, they aren't available";
-
-      var suv = {
-        price:50,
-        availability:4
-      };
-        if (suv.availability > 0) {
-          return "Yes, they are available";
-      }
-        else return "No, they aren't available";
-  }
-    
-     var sports = {
-      price:25,
-      availability:0
-     };
-        if (sports.availability > 0) {
-        return "Yes, they are available";
+      
+     if (sedan.availability >= 1)  {
+          return "Yes, they are available"; 
         }
-        else return "No, they aren't available";
+    else  {
+         "No, they aren't available";
+        } 
+/* I don't think it's getting to this part of the code at all, but I don't know why 
+I'm sure it's something small so that's why they're coming up undefined and only sedan is working*/
+
+    var suv = {
+        price:50,
+        availability:0
+      };
+      
+    if (suv.availability >= 1) {
+          return "Yes, they are available";
+        }
+    else  { 
+           "No, they aren't available";
+        }
     
+    var sports = {
+        price:25,
+        availability:0
+     };
+     
+    if (sports.availability >= 1) {
+          return "Yes, they are available";
+        }
+    else  { 
+          return "No, they aren't available";
+        }
 }
