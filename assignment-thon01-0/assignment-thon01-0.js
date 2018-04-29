2d9f960cb97266d675c7bf7968273895006e8d4e
Funmi <folowookere@yahoo.com>
2018-04-29 04:53:49 +0000

    Victory with a few more fixes

--- assignment-thon01-0/assignment-thon01-0.js
+++ assignment-thon01-0/assignment-thon01-0.js
@@ -16,7 +16,7 @@ var carDealership = {
      
      checkAvailability(typeOfCar) {
     if (this[typeOfCar].availability > 0) {
-          return `Yes, there are  ${this[typeOfCar].availability} ${typeOfCar}'s available.`;
+          return `Yes, there are ${this[typeOfCar].availability} ${typeOfCar}'s available. Each cost ${this[typeOfCar].price}.`;
         }
          return `There are no ${typeOfCar}'s available.`;
         }
